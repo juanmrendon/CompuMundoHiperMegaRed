c90e5d79d0cad367aa269b68bf026d3d938f23bf
Fabian Dominguez <fdominguezd@gmail.com>
2021-09-30 19:23:53 -0500

    In Process. Frontend

--- Project.App/Project.App.Consola/Program.cs
+++ Project.App/Project.App.Consola/Program.cs
@@ -6,9 +6,39 @@ namespace Project.App.Consola
 {
     class Program
     {
+        private static IRepositorioProfesor _repoProfesor = new RepositorioProfesor(new Persistencia.AppContext());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            AddProfesor();  
+            //BuscarProfesor(1);
+            //BuscarProfesor(2); 
+            //EliminarProfesor(4); 
+            //BuscarProfesores()         
         }
+
+
+        //AddProfesor
+
+        private static void AddProfesor()
+        {
+            var profesor = new Profesor
+            {  
+               nombre = "Fabián",
+               apellidos = "Dominguez",
+               identificacion = "1000566777",
+               edad = 32,
+               diagnostico = null,
+               vacuna = null,
+               departamento = "Humanidades",
+               materia = "Metodología de la Investigación"
+            };
+
+            Console.WriteLine("Nombre: " + profesor.nombre + "\n Apellido = " + profesor.apellidos);
+            Profesor profesor_retornado = _repoProfesor.AddProfesor(profesor); 
+            if (profesor_retornado!=null)
+                Console.WriteLine("Se registró un profesor en la base de datos");
+        }
+
     }
 }
